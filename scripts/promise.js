46cbad41349975af225e5dbfcf3d39f045439242
Geoff Cox <redgeoff@gmail.com>
2017-07-03 14:59:36 -0700

    fix(bluebird): disable warnings

--- scripts/promise.js
+++ scripts/promise.js
@@ -2,4 +2,11 @@
 
 // A wrapper that can be discarded once Promises become standard across all recent browser versions
 
-module.exports = require('bluebird');
+var Promise = require('bluebird');
+
+// Turn off the warnings as they are often reported when they should not
+Promise.config({
+  warnings: false
+});
+
+module.exports = Promise;
