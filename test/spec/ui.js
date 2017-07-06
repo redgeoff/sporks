f3eacfea83bc05c8447721b4348913b0f396e174
Geoff Cox <redgeoff@gmail.com>
2017-07-06 07:20:32 -0700

    test(ui): absolutePosition

--- test/spec/ui.js
+++ test/spec/ui.js
@@ -1,5 +1,18 @@
 'use strict';
 
+var sporks = require('../../scripts/browser');
+
 describe('ui', function () {
+
+  it('should calculate absolute position', function () {
+    var parentDiv = document.createElement('div'),
+      div = document.createElement('div');
+    parentDiv.style.position = 'absolute';
+    parentDiv.style.top = '5px';
+    parentDiv.style.left = '5px';
+    parentDiv.appendChild(div);
+    document.body.append(parentDiv);
+    sporks.absolutePosition(div).should.eql({ x: 5, y: 5 })
+  });
 
 });
